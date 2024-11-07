81e29f8332d5acd5d65a4100aefc87b4f58c46bd
Fahridanaa <fahridanaaa@gmail.com>
2024-11-07 20:48:51 +0700

    feat: add rounded button widget

---
+++ lib/widgets/rounded_button.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:leafolyze/constants/color.dart';
+
+class RoundedButton extends StatelessWidget {
+  final String text;
+
+  const RoundedButton({super.key, required this.text});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      width: double.infinity,
+      child: ElevatedButton(
+        onPressed: () {},
+        style: ElevatedButton.styleFrom(
+          backgroundColor: AppColors.primaryColor,
+          padding: const EdgeInsets.symmetric(vertical: 12, horizontal: 20),
+          shape: RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(50),
+          ),
+        ),
+        child: Text(
+          text,
+          style: const TextStyle(
+              fontSize: 16,
+              color: AppColors.primaryForegroundColor,
+              fontWeight: FontWeight.w600),
+        ),
+      ),
+    );
+  }
+}
