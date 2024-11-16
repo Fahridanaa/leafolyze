c6d0695a3e454c384dca0e2e8d00d3585a3b5439
Fahridanaa <fahridanaaa@gmail.com>
2024-11-16 14:15:23 +0700

    feat: add BLoC to product list screen

---
+++ lib/blocs/marketplace/marketplace_event.dart
@@ -0,0 +1,37 @@
+import 'package:equatable/equatable.dart';
+
+abstract class MarketplaceEvent extends Equatable {
+  const MarketplaceEvent();
+
+  @override
+  List<Object?> get props => [];
+}
+
+class LoadProducts extends MarketplaceEvent {}
+
+class SearchProducts extends MarketplaceEvent {
+  final String query;
+
+  const SearchProducts(this.query);
+
+  @override
+  List<Object?> get props => [query];
+}
+
+class SortProducts extends MarketplaceEvent {
+  final String sortOption;
+
+  const SortProducts(this.sortOption);
+
+  @override
+  List<Object?> get props => [sortOption];
+}
+
+class FilterProducts extends MarketplaceEvent {
+  final String filterOption;
+
+  const FilterProducts(this.filterOption);
+
+  @override
+  List<Object?> get props => [filterOption];
+}
---
+++ lib/repositories/marketplace_repository.dart
@@ -0,0 +1,21 @@
+import 'package:leafolyze/models/product.dart';
+import 'package:leafolyze/services/api_service.dart';
+
+class MarketplaceRepository {
+  final ApiService _apiService;
+
+  MarketplaceRepository(this._apiService);
+
+  Future<List<Product>> getProducts() async {
+    try {
+      final response = await _apiService.get('/products');
+      if (response['success']) {
+        final List<dynamic> productsJson = response['data'];
+        return productsJson.map((json) => Product.fromJson(json)).toList();
+      }
+      throw Exception(response['message']);
+    } catch (e) {
+      throw Exception('Failed to load products: $e');
+    }
+  }
+}
